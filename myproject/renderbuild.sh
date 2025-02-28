078d4f1ea8664105d8cf8805c6e42fc9163835e1
JadavDurgesh <109644031+JadavDurgesh@users.noreply.github.com>
2025-02-28 10:40:51 +0530

    Update renderbuild.sh

--- myproject/renderbuild.sh
+++ myproject/renderbuild.sh
@@ -3,16 +3,16 @@ set -e  # Exit on error
 
 echo "Updating system packages..."
 apt-get update && apt-get install -y tesseract-ocr libtesseract-dev libgl1
-ls
+
 echo "Installing Python dependencies..."
 cd myproject
-ls
+
 python -m pip install --upgrade pip
-pip install -r requirement.txt
 ls
+pip install -r requirement.txt
+
 echo "Downloading model from Google Drive..."
 cd myapp
-pip install gdown
 gdown https://drive.google.com/uc?id=1UXU1FV6ZNuB0HOJDrledlOjdkRNxLMZv
 cd ..
 
