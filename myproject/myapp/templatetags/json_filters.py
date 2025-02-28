e0e3eadd3b2f3169454619a8a332b2ece1be1f8c
JadavDurgesh <109644031+JadavDurgesh@users.noreply.github.com>
2025-02-28 21:23:38 +0530

    Create json_filters.py

---
+++ myproject/myapp/templatetags/json_filters.py
@@ -0,0 +1,18 @@
+import json
+from django import template
+
+register = template.Library()
+
+@register.filter
+def json_pretty(value):
+    """Pretty prints JSON data."""
+    try:
+        return json.dumps(value, indent=4)
+    except Exception:
+        return value
+
+
+@register.filter
+def get_item(dictionary, key):
+    """Retrieve a value from a dictionary using the given key."""
+    return dictionary.get(key, "")
