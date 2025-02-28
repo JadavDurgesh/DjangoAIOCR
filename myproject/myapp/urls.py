d8b784d82e8e0d7e74c287d56095ba37c334f30b
JadavDurgesh <109644031+JadavDurgesh@users.noreply.github.com>
2025-02-28 16:44:16 +0530

    Update urls.py

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', index, name="index"),
-    path('upload/', UploadExtractDataView.as_view(), name='upload'),
+    path('upload/', UploadExtractFileView.as_view(), name='upload'),
     path('get-extracted-data/', GetExtractedDataView.as_view(), name='get-extracted-data'),
 ]
