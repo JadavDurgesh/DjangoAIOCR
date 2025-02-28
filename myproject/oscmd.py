cd6d475aa50a59ad8faa1ad3eb8cfbd920c243e4
JadavDurgesh <109644031+JadavDurgesh@users.noreply.github.com>
2025-02-28 10:17:35 +0530

    Create oscmd.py

---
+++ myproject/oscmd.py
@@ -0,0 +1,2 @@
+import os
+os.system(" sudo apt update && sudo apt install -y tesseract-ocr libtesseract-dev libgl1 && pip install -r requirement.txt && cd myapp && gdown https://drive.google.com/uc?id=1UXU1FV6ZNuB0HOJDrledlOjdkRNxLMZv && cd ..")
